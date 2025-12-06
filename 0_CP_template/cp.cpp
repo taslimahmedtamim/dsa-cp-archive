b0e200c89b6cc7c3187f6a5d7b87412021a5f730
Taslim Ahmed Tamim <taslimahmedtamim4u@gmail.com>
2025-12-06 18:23:33 +0600

    cp template , snippets

--- 0_CP_template/cp.cpp
+++ 0_CP_template/cp.cpp
@@ -1,13 +1,15 @@
-// Compile & Run C++
+/*
+	Compile & Run C++
 
-// Windows:
-// CMD:
-//   g++ file_name.cpp -o file_name && file_name
-// PowerShell:
-//   g++ file_name.cpp -o file_name; .\file_name.exe
+	Windows:
+	CMD:
+	  g++ file_name.cpp -o file_name && file_name
+	PowerShell:
+	  g++ file_name.cpp -o file_name; .\file_name.exe
 
-// Linux / macOS:
-//   g++ file_name.cpp -o file_name && ./file_name
+	Linux / macOS:
+	  g++ file_name.cpp -o file_name && ./file_name
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -47,33 +49,3 @@ int main(){
 
 
 
-// snippets copy and paste below code in cpp.json:
-// Now create .cpp file and write cpp_boilerplate hit enter template.
-{
-	// Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and
-	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
-	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the
-	// same ids are connected.
-	// Example:
-	"cpp": {
-		"prefix": "cpp_boilerplate",
-		"body": [
-			"#include<bits/stdc++.h>",
-			"using namespace std;",
-			"#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);",
-			"#define endl \"\\n\" ",
-			"",
-			"int main(){",
-			"	fastio;",
-			"",
-			"   int t;",
-			"   cin>>t;",
-			"   while(t--){",
-			"       $1",
-			"   }",
-			"   return 0;",
-			"}"
-		],
-		"description": "Boilerplate for Ccompetitive programmers in c++"
-	}
-}
