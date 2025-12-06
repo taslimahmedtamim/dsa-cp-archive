fb238275046cbbf8ad37b794ef111e5f7a9b3db1
Taslim Ahmed Tamim <taslimahmedtamim4u@gmail.com>
2025-12-06 18:09:50 +0600

    Refactor C++ template and add file I/O support

--- 0_CP_template/cp.cpp
+++ 0_CP_template/cp.cpp
@@ -1,21 +1,54 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-// #define endl "\n"
-// #define ll long long
+// Compile & Run C++
 
-// int main(){
-//     fastio;
+// Windows:
+// CMD:
+//   g++ file_name.cpp -o file_name && file_name
+// PowerShell:
+//   g++ file_name.cpp -o file_name; .\file_name.exe
 
-//     int t;
-//     cin>>t;
-//     while(t--){
-        
-//     }
-//     return 0;
-// }
+// Linux / macOS:
+//   g++ file_name.cpp -o file_name && ./file_name
+
+#include<bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define MOD 1000000007
+
+void fastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void fileIO(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve(){
+    // Write your code here
+}
+
+int main(){
+
+    fastIO();
+    fileIO();
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+}
 
 
+
+// snippets copy and paste below code in cpp.json:
+// Now create .cpp file and write cpp_boilerplate hit enter template.
 {
 	// Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and
 	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
@@ -31,7 +64,7 @@
 			"#define endl \"\\n\" ",
 			"",
 			"int main(){",
-			"   fastio;",
+			"	fastio;",
 			"",
 			"   int t;",
 			"   cin>>t;",
